654f520e2efb333759609945cf412afd36f0dc17
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-07-05 15:13:58 -0300

    Add navbar animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,20 @@
 const menuBtn = document.querySelector('.menu-btn')
 let menuOpen = false
+
+function navLinkAnimation() {
+    const navLinks = document.querySelectorAll('li')
+    navLinks.forEach(function(link) {
+        /*Verificando se existe a propriedade animation em cada li no html. Se já existe, deixa vazio. Se não, cria.*/
+        link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`)
+    })
+    
+}
+
 menuBtn.addEventListener('click', () => {
+    const navbar = document.querySelector('ul')
+    navbar.classList.toggle('active')
     if(!menuOpen) {
         menuBtn.classList.add('open')
         menuOpen = true
@@ -8,4 +22,5 @@ menuBtn.addEventListener('click', () => {
         menuBtn.classList.remove('open')
         menuOpen = false
     }
+    navLinkAnimation()
 })
